ef07aa8a8d94756e1f0092ce3491cae7911bdda3
Zenith-hasan <abuhasan323382@gmail.com>
2025-02-18 12:53:00 +0600

    function basics

---
+++ 01_Basics/01_DataType.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+// data type - integer
+int x = 10;
+cout << "The Value of x is : "<<x << '\n';
+
+// data type - float
+float y = 3.77;
+cout <<"The value of y is: " << y << endl;
+
+
+return 0;
+}
+
+
---
+++ Function/Basic.cpp
@@ -0,0 +1,19 @@
+
+//? A function is a set of statements that takes input,does some specific computation and produce output.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// writing a function that give the maximum of a number
+
+int max(int x , int y ){
+	if (x > y)
+		return x;
+	else 
+		return y;
+}
+int main ( ){
+    cout << "Hello World!" << endl;
+    return 0;
+}
